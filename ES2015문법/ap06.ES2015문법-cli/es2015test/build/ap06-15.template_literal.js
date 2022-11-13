de9a7d709bd1f80a9afd380e276968d4a6fa4f98
sejoong <sjydevil@gmail.com>
2022-11-13 11:42:55 +0900

    2022-11-13 11:42

--- ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-15.template_literal.js
@@ -1,6 +1,14 @@
+"use strict";
+
 /*
 
-    ES2015의 template literal( 백틱, ``) 에 대해서 알아본다.
-    
+    ES2015의 template literal( 백틱, ` ) 에 대해서 알아본다.
+    template literal 의 용도
+    1. 다중 문자열 만들 때
+    2. 변수 치환
+
 */
-"use strict";
+var name = '홍길동';
+var age = 20;
+var str = "\n  abc ".concat(name, "\n  def\n  kfa\n");
+console.log(str);
--- ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/src/ap06-15.template_literal.js
@@ -1,5 +1,21 @@
 /*
 
-    ES2015의 template literal( 백틱, ``) 에 대해서 알아본다.
-    
+    ES2015의 template literal( 백틱, ` ) 에 대해서 알아본다.
+    template literal 의 용도
+    1. 다중 문자열 만들 때
+    2. 변수 치환
+
 */
+const name = '홍길동';
+const age = 20;
+const str = `
+  abc ${name}
+  def
+`;
+console.log(str);
+
+const product = { name: '도서', price: '4200원' };
+const message = `
+  제품 ${product.name}의 가격은 ${product.price}입니다
+`;
+console.log(message);
