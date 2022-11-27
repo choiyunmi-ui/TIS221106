7fabace88ca3de2dcca7ca4e90b43da2cced62fe
sejoong <sjydevil@gmail.com>
2022-11-27 14:58:45 +0900

    2022-11-27 14:58

--- Vuejs/vue30컴포넌트실습/comprj01/src/views/TodoView.vue
+++ Vuejs/vue30컴포넌트실습/comprj01/src/views/TodoView.vue
@@ -21,17 +21,21 @@ button {
     <TodoHeader></TodoHeader>
 
     <!-- TodoInput -->
+    <TodoInput></TodoInput>
 
     <!-- TodoList -->
 
     <!-- TodoFooter -->
+    <TodoFooter v-on:clearAll="clearAll"></TodoFooter>
   </div>
 </template>
 
 <script>
 // vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
 // import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
-import TodoHeader from "../components/todo/TodoHeader.vue";
+import TodoHeader from '../components/todo/TodoHeader.vue';
+import TodoFooter from '../components/todo/TodoFooter.vue';
+import TodoInput from '../components/todo/TodoInput.vue';
 
 export default {
   /* pdtmc^2w */
@@ -43,6 +47,10 @@ export default {
   //template: ``,
   methods: {
     /* 이벤트 핸들러 등록 + 일반 함수 */
+    clearAll(e) {
+      debugger;
+      console.log(e.target);
+    },
     /* vuex 를 사용하는 경우
       mapActions 는 store의 actions 를 가져오는 헬퍼 메서드니다.
       namespaced: true를 설정한 경우 네임스페이스를 사용하기 때문에 store의 모듈 명을 적어주어야 합니다.
@@ -56,7 +64,9 @@ export default {
   components: {
     /* 전역 컴포넌트인 경우는 등록하지 않는다. 전역 컴포넌트는 프로토타입 체인으로 찾을 수 있기 때문에 */
     /* 지역 컴포넌트나 파일 컴포넌트만 등록 한다. 예시) "태그명" : 컴포넌트명 */
-    'TodoHeader': TodoHeader,
+    TodoHeader: TodoHeader,
+    TodoFooter: TodoFooter,
+    TodoInput: TodoInput,
   },
   computed: {
     /* 자동처리 + 동기식. 메서드로 작성. return 필수. data 와 공존 불가 */
